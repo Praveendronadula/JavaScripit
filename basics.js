73973bc42ad4577dc30e881e90902a460eec586b
Praveen <praveendronadula143@gmail.com>
2026-05-11 20:52:25 +0530

    first commit

---
+++ basics.js
@@ -0,0 +1,128 @@
+// let a =1;
+// let b=2;
+// let c=a+b;
+// console.log(c);
+// console.log(typeof(c));
+// let d=c++;
+// console.log(d)
+// // // console.log("hello_world")
+// // // var a = "12";
+// // // var b = 13;
+// // // console.log(typeof a);
+// // // {
+// // //     var c = 2
+
+// // // }
+// // // console.log(c);
+// // // //Examples of ressign
+// // // // var example
+// // // var x = 1;
+// // // var x = 2; // Allowed
+
+
+// // // // let example
+// // // let y = 1;
+// // // // let y = 2; // Throws SyntaxError
+
+// // // // const example
+// // // const z = 1;
+// // // // z = 2; // Throws TypeError
+// // // console.log(x)
+// // // console.log(y)
+// // // console.log(z)
+// // // {
+// // //     var p = 0;
+// // //     var p = 33;
+// // //     console.log(p);
+
+// // // }
+// // // // console.log(o);
+// // // let o = 5;
+// // // const l = "name"
+// // // console.log(l);
+
+// // // var car = "kia";
+// // // // const price = 10000; throgh a error that  if uh use const value haven't change
+// // // let price=10000;
+// // // price++
+// // // console.log(car +"value is"+price);
+// // // console.log( typeof car +"value is"+ typeof price);
+// // // q="8";
+// // // w=3;
+// // // q**=w;
+// // // console.log(typeof q)
+// // // console.log(q)
+// // // var a =3;
+// // // var b=4;
+// // // console.log(a==b);
+// // // console.log(a<=b);
+// // // console.log(a!=b);
+// // // console.log(a===3);
+// // // console.log(a!==b);
+// // // let a=2;
+// // // let b=3;
+// // // console.log(a>1 &&b==0);
+// // // var a=4;
+// // // var b=6;
+// // // a==b?console.log("a is big"):console.log("both are noequal");
+// // // let c
+// // // c= "3"*"2"
+// // // console.log(c);
+// // // let  a="3.89";
+// // // console.log(a);
+// // // console.log(typeof(a));
+// // // b=parseInt(a);
+// // // console.log(b)
+// // // console.log(typeof(b));
+// // // let age = 9
+// // // if (age == 18) {
+// // //     console.log("the person is adult");
+// // // }
+// // // else if (age >= 18) {
+// // //     console.log("the person is adult");
+// // // }
+// // // else {
+// // //     console.log("the person is minor")
+// // // }
+// // let a =1;
+// // for(i=a;i<=10;i++){
+// //     console.log(i)
+// // }
+// // for (let i = 1; i <= 20; i++) {
+// //     if (i % 2 == 0) {
+// //         console.log(i);
+// //     }
+// // }
+// // for (let i = 10; i >= 1; i--) {
+// //     console.log(i);
+// // }/
+// let marks=66
+// let grade;
+// if (marks>=80){
+//     grade="A";
+// }
+// else if (marks>=50){
+//     grade="B";
+// }
+// else{
+//     grade="C"
+// }
+// switch(grade){
+//     case "A":
+//         console.log("A")
+//         break;
+//          case "B":
+//         console.log("B")
+//         break;
+//          case "C":
+//         console.log("C")
+//         break;
+
+// }   
+
+let a="JAVASCRIPIT"
+console.log(a.length);
+console.log(a.indexOf("S"));
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+console.log(a.charAt(4));
