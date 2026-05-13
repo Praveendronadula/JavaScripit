32507e02e0cd480a3dadab911059d72c30fc9f81
Praveen <praveendronadula143@gmail.com>
2026-05-13 11:07:12 +0530

    control flow

--- control_flow.js
+++ control_flow.js
@@ -1,11 +1,64 @@
-let marks=66
+let mark=66
 let grade;
-if (marks>=80){
+if (mark>=80){
     grade="A";
 }
-else if (marks>=50){
+else if (mark>=50){
   console.log(  grade="B");
 }
 else{
       console.log(  grade="C");
 }
+var a=99;
+while(a<100){
+    console.log(a);
+    a++;
+}
+let b=2;
+let c=3;
+do{
+    console.log(b,c);//2,3
+    b++;//3
+}while(b==c);3,3
+
+var marks = 90;
+switch (true) {
+
+    case marks < 30:
+        console.log("Fail");
+        break;
+
+    case marks < 50:
+        console.log("Grade C");
+        break;
+
+    case marks < 70:
+        console.log("Grade B");
+        break;
+
+    default:
+        console.log("Grade A");
+}
+let Marks=66
+let Grade;
+if (Marks>=80){
+    Grade="A";
+}
+else if (Marks>=50){
+    Grade="B";
+}
+else{
+    Grade="C"
+}
+switch(Grade){
+    case "A":
+        console.log("A")
+        break;
+         case "B":
+        console.log("B")
+        break;
+         case "C":
+        console.log("C")
+        break;
+
+}   
